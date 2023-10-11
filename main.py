a1bf06fd3694f490bcd07ee7afc61e80369cf022
tayleredelen <taylerledelen@gmail.com>
2023-10-11 16:43:01 -0500

    Day 26: updated font size and color, removed cell border

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
-    pdf.set_font(family="Times", style="B", size=12)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=1)
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(0, 0, 254)
+    # makes text gray
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
 
 pdf.output("topics.pdf")
