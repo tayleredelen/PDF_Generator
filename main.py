6b679c90b4635d1bb295610f2171c77912af5a79
tayleredelen <taylerledelen@gmail.com>
2023-10-12 12:10:23 -0500

    Day 26: added lines to pages

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
+
     # sets header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     # makes text gray
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(x1=10, y1=21, x2=200, y2=21)
+
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 #     x1 value = distance from left border to left edge of pdf in defined unit (mm)
 #     y1 value = distance from left border to top of pdf in defined unit (mm)
 #     x2 value = distance from right border to right edge of pdf in defined unit (mm)
 #     y2 value = distance from right border to top of pdf in defined unit (mm)
 
+    # sets footer
     # sets footer
     pdf.ln(265)
     # paramater is height in selected unit (mm) places text at bottom of page
@@ -37,5 +41,7 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("topics.pdf")
