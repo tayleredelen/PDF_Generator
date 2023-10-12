b3d9dc57d58eb70c0b0cb29cfe5d6e9305ce18f7
tayleredelen <taylerledelen@gmail.com>
2023-10-12 11:41:14 -0500

    Day 26: added line under titles

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(0, 0, 254)
+    pdf.set_text_color(100, 100, 100)
     # makes text gray
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(x1=10, y1=21, x2=200, y2=21)
+#     x1 value = distance from left border to left edge of pdf in defined unit (mm)
+#     y1 value = distance from left border to top of pdf in defined unit (mm)
+#     x2 value = distance from right border to right edge of pdf in defined unit (mm)
+#     y2 value = distance from right border to top of pdf in defined unit (mm)
 
 pdf.output("topics.pdf")
